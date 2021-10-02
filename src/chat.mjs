3df5e9cf6e2b598b8697a41e00715ecedd0033a2
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-10-01 20:41:57 -0700

    Hardcode chat room = test for now

--- src/chat.mjs
+++ src/chat.mjs
@@ -126,72 +126,36 @@ async function handleApiRequest(path, request, env) {
 
       if (!path[1]) {
         // The request is for just "/api/room", with no ID.
-        if (request.method == "POST") {
-          // POST to /api/room creates a private room.
-          //
-          // Incidentally, this code doesn't actually store anything. It just generates a valid
-          // unique ID for this namespace. Each durable object namespace has its own ID space, but
-          // IDs from one namespace are not valid for any other.
-          //
-          // The IDs returned by `newUniqueId()` are unguessable, so are a valid way to implement
-          // "anyone with the link can access" sharing. Additionally, IDs generated this way have
-          // a performance benefit over IDs generated from names: When a unique ID is generated,
-          // the system knows it is unique without having to communicate with the rest of the
-          // world -- i.e., there is no way that someone in the UK and someone in New Zealand
-          // could coincidentally create the same ID at the same time, because unique IDs are,
-          // well, unique!
-          let id = env.rooms.newUniqueId();
-          return new Response(id.toString(), {headers: {"Access-Control-Allow-Origin": "*"}});
-        } else {
-          // If we wanted to support returning a list of public rooms, this might be a place to do
-          // it. The list of room names might be a good thing to store in KV, though a singleton
-          // Durable Object is also a possibility as long as the Cache API is used to cache reads.
-          // (A caching layer would be needed because a single Durable Object is single-threaded,
-          // so the amount of traffic it can handle is limited. Also, caching would improve latency
-          // for users who don't happen to be located close to the singleton.)
-          //
-          // For this demo, though, we're not implementing a public room list, mainly because
-          // inevitably some trolls would probably register a bunch of offensive room names. Sigh.
+        // if (request.method == "POST") {
+        //   // POST to /api/room creates a private room.
+        //   let id = env.rooms.newUniqueId();
+        //   return new Response(id.toString(), {headers: {"Access-Control-Allow-Origin": "*"}});
+        // } else {
+        //   // List rooms
           return new Response("Method not allowed", {status: 405});
-        }
+        // }
       }
 
       // OK, the request is for `/api/room/<name>/...`. It's time to route to the Durable Object
       // for the specific room.
       let name = path[1];
+      name = "test";  // FIXME: for now, only 1 document called "test"
 
       // Each Durable Object has a 256-bit unique ID. IDs can be derived from string names, or
       // chosen randomly by the system.
       let id;
       if (name.match(/^[0-9a-f]{64}$/)) {
-        // The name is 64 hex digits, so let's assume it actually just encodes an ID. We use this
-        // for private rooms. `idFromString()` simply parses the text as a hex encoding of the raw
-        // ID (and verifies that this is a valid ID for this namespace).
         id = env.rooms.idFromString(name);
       } else if (name.length <= 32) {
-        // Treat as a string room name (limited to 32 characters). `idFromName()` consistently
-        // derives an ID from a string.
         id = env.rooms.idFromName(name);
       } else {
         return new Response("Name too long", {status: 404});
       }
 
-      // Get the Durable Object stub for this room! The stub is a client object that can be used
-      // to send messages to the remote Durable Object instance. The stub is returned immediately;
-      // there is no need to await it. This is important because you would not want to wait for
-      // a network round trip before you could start sending requests. Since Durable Objects are
-      // created on-demand when the ID is first used, there's nothing to wait for anyway; we know
-      // an object will be available somewhere to receive our requests.
+      // Get the Durable Object stub for this room!
       let roomObject = env.rooms.get(id);
-
-      // Compute a new URL with `/api/room/<name>` removed. We'll forward the rest of the path
-      // to the Durable Object.
       let newUrl = new URL(request.url);
       newUrl.pathname = "/" + path.slice(2).join("/");
-
-      // Send the request to the object. The `fetch()` method of a Durable Object stub has the
-      // same signature as the global `fetch()` function, but the request is always sent to the
-      // object, regardless of the request's URL.
       return roomObject.fetch(newUrl, request);
     }
 
@@ -208,15 +172,13 @@ async function handleApiRequest(path, request, env) {
 // to all others.
 export class ChatRoom {
   constructor(controller, env) {
-    // `controller.storage` provides access to our durable storage. It provides a simple KV
-    // get()/put() interface.
+    // `controller.storage` provides access to our durable storage. It provides a simple KV get()/put() interface.
     this.storage = controller.storage;
 
     // `env` is our environment bindings (discussed earlier).
     this.env = env;
 
-    // We will put the WebSocket objects for each client, along with some metadata, into
-    // `sessions`.
+    // We will put the WebSocket objects for each client, along with some metadata, into `sessions`.
     this.sessions = [];
 
     // We keep track of the last-seen message's timestamp just so that we can assign monotonically
@@ -236,26 +198,13 @@ export class ChatRoom {
 
       switch (url.pathname) {
         case "/websocket": {
-          // The request is to `/api/room/<name>/websocket`. A client is trying to establish a new
-          // WebSocket session.
+          // The request is to `/api/room/<name>/websocket`. A client is trying to establish a new WebSocket session.
           if (request.headers.get("Upgrade") != "websocket") {
             return new Response("expected websocket", {status: 400});
           }
-
-          // Get the client's IP address for use with the rate limiter.
           let ip = request.headers.get("CF-Connecting-IP");
-
-          // To accept the WebSocket request, we create a WebSocketPair (which is like a socketpair,
-          // i.e. two WebSockets that talk to each other), we return one end of the pair in the
-          // response, and we operate on the other end. Note that this API is not part of the
-          // Fetch API standard; unfortunately, the Fetch API / Service Workers specs do not define
-          // any way to act as a WebSocket server today.
           let pair = new WebSocketPair();
-
-          // We're going to take pair[1] as our end, and return pair[0] to the client.
           await this.handleSession(pair[1], ip);
-
-          // Now we return the other end of the pair to the client.
           return new Response(null, { status: 101, webSocket: pair[0] });
         }
 
@@ -267,8 +216,6 @@ export class ChatRoom {
 
   // handleSession() implements our WebSocket-based chat protocol.
   async handleSession(webSocket, ip) {
-    // Accept our end of the WebSocket. This tells the runtime that we'll be terminating the
-    // WebSocket in JavaScript, not sending it elsewhere.
     webSocket.accept();
 
     // Set up our rate limiter client.
@@ -278,8 +225,6 @@ export class ChatRoom {
         err => webSocket.close(1011, err.stack));
 
     // Create our session and add it to the sessions list.
-    // We don't send any messages to the client until it has sent us the initial user info
-    // message. Until then, we will queue messages in `session.blockedMessages`.
     let session = {webSocket, blockedMessages: []};
     this.sessions.push(session);
 
@@ -290,8 +235,7 @@ export class ChatRoom {
       }
     });
 
-    // Load the last 100 messages from the chat history stored on disk, and send them to the
-    // client.
+    // Load the last 100 messages from the chat history stored on disk, and send them to the client.
     let storage = await this.storage.list({reverse: true, limit: 100});
     let backlog = [...storage.values()];
     backlog.reverse();
