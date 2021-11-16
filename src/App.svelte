fffd0cc624d25cd54988ace0d672360b6de90f30
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-11-16 12:16:00 -0800

    Allow user to enter their name and save it in localstorage

--- src/App.svelte
+++ src/App.svelte
@@ -5,34 +5,52 @@ import { onMount } from "svelte";
 import IGV from "./IGV.svelte";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-let username = `${ulid()}:robert`;
+let username = localStorage.getItem("user");  // localStorage is synchronous
 let roomname = new URL(window.location).searchParams.get("room");
 let disabled = false;
 let rooms = [];
+let usernameNew;
 
 // Create a new room and redirect to it
 async function createRoom() {
 	disabled = true;
 	const newRoomID = await fetch("/api/rooms", { method: "POST"}).then(d => d.text());
-	window.location = `?room=${newRoomID}`;
+
+	const url = new URL(window.location);
+	url.searchParams.set("room", newRoomID);
+	window.location = String(url);
 }
 
-// On load
+// Save user's name
+async function saveUser(name) {
+	if(!name)
+		return;
+	username = `${ulid()}:${name}`;
+	localStorage.setItem("user", username);
+}
+
+// On load, add room to local history if not seen before
 onMount(async () => {
-	// Add room to local history if not seen before
 	rooms = await localforage.getItem("rooms") || [];
 	if(roomname != "" && roomname != null && !rooms.includes(roomname)) {
 		await localforage.setItem("rooms", rooms.concat([ roomname ]));
 	}
-})
+});
 </script>
 
-{#if roomname}
+{#if roomname && username}
 	<IGV {username} {roomname} />
+{:else if !username}
+	<div class="input-group mb-3">
+		<span class="input-group-text" id="input-name">Your name:</span>
+		<input type="text" class="form-control" aria-label="Username" aria-describedby="input-name" bind:value={usernameNew}>
+	</div>
+
+	<button class="btn btn-outline-success mt-4" on:click={() => saveUser(usernameNew)} {disabled}>Save</button>
 {:else}
 	<h5 class="mt-3">Recently viewed rooms</h5>
 	{#if rooms.length == 0}
-		<span class="text-muted">None</span>
+		<span class="text-muted">None</span><br />
 	{:else}
 		{#each rooms as room}
 			<a href="?room={room}">{room}</a><br />
