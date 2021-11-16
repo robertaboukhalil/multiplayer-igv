1d8df7defc4c8f873b37fe2c4f5ce666b4a29ad0
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-11-16 11:34:04 -0800

    Add ability to create room in UI

--- src/App.svelte
+++ src/App.svelte
@@ -5,10 +5,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 let username = `${ulid()}:robert`;
 let roomname = new URL(window.location).searchParams.get("room");
+let disabled = false;
+
+// Create a new room and redirect to it
+async function createRoom() {
+	disabled = true;
+	const newRoomID = await fetch("/api/rooms", { method: "POST"}).then(d => d.text());
+	window.location = `?room=${newRoomID}`;
+}
 </script>
 
 {#if roomname}
 	<IGV {username} {roomname} />
 {:else}
-	[ Create a new room ]
+	<button on:click={createRoom} {disabled}>Create a new room</button>
 {/if}
