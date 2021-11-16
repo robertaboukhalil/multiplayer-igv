035ea0d4db6331ec0eaa0adb009071c9cd1861c9
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-11-16 11:48:00 -0800

    Small fix

--- src/IGV.svelte
+++ src/IGV.svelte
@@ -296,7 +296,7 @@ handlePointerLeave = debounce(handlePointerLeave, 10);
 
 		<h5>Connected Users</h5>
 		{#each Object.keys(cursors) as name}
-			{#if cursors[name].timestamp == null || new Date().getTime() - cursors[name].timestamp < 100000}
+			{#if cursors[name].timestamp == null || new Date().getTime() - cursors[name].timestamp < 1000000}
 				<span style="color: {getColor(name)}">&bullet;</span> {name.split(":")[1]}
 				{#if name === username}
 					<strong>(me)</strong>
