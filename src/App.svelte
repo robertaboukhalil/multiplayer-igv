686d0ad1d1860ee42e8fb349deb2bcc52a19754d
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-11-16 11:45:22 -0800

    Add list of recently viewed rooms

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,14 @@
 <script>
 import { ulid } from "ulid";
+import localforage from "localforage";
+import { onMount } from "svelte";
 import IGV from "./IGV.svelte";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 let username = `${ulid()}:robert`;
 let roomname = new URL(window.location).searchParams.get("room");
 let disabled = false;
+let rooms = [];
 
 // Create a new room and redirect to it
 async function createRoom() {
@@ -13,10 +16,28 @@ async function createRoom() {
 	const newRoomID = await fetch("/api/rooms", { method: "POST"}).then(d => d.text());
 	window.location = `?room=${newRoomID}`;
 }
+
+// On load
+onMount(async () => {
+	// Add room to local history if not seen before
+	rooms = await localforage.getItem("rooms") || [];
+	if(roomname != "" && roomname != null && !rooms.includes(roomname)) {
+		await localforage.setItem("rooms", rooms.concat([ roomname ]));
+	}
+})
 </script>
 
 {#if roomname}
 	<IGV {username} {roomname} />
 {:else}
 	<button on:click={createRoom} {disabled}>Create a new room</button>
+
+	<h5 class="mt-3">Recently viewed</h5>
+	{#if rooms.length == 0}
+		<span class="text-muted">None</span>
+	{:else}
+		{#each rooms as room}
+			<a href="?room={room}">{room}</a><br />
+		{/each}
+	{/if}
 {/if}
