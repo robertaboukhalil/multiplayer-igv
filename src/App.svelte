c2f8d83820543d1fb870d26f546367ecabdb03d1
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-11-16 11:33:45 -0800

    Add bootstrap

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
 import { ulid } from "ulid";
 import IGV from "./IGV.svelte";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 let username = `${ulid()}:robert`;
 let roomname = new URL(window.location).searchParams.get("room");
